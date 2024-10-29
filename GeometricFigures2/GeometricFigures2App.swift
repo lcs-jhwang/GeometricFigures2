44b8bcaf7ccb3241758b1668abb70fc903f2f2e0
Julien Hwang <jhwang@lcs.on.ca>
2024-10-29 11:54:35 -0400

    Initial Commit

---
+++ GeometricFigures2/GeometricFigures2App.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFigures2App.swift
+//  GeometricFigures2
+//
+//  Created by Julien Hwang on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFigures2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
